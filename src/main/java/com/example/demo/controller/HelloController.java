04aa0d410eab168b961b47a0a552fcba25c2a92d
Kukko Miikka <a1700386@myy.haaga-helia.fi>
2018-01-17 11:26:37 +0200

    third commit

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @ResponseBody
 public class HelloController {
-	@RequestMapping("*")
-	public String hello() {
-		return "Hello Spring";
+	@RequestMapping("/index")
+	public String index() {
+		return "This is the main page";
+	}
+	
+	@RequestMapping("/contact")
+	public String contact() {
+		return "This is the contact page";
 	}
 
 }
