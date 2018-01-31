4bc15a6a810ae85d7c4abfbb13f8dc1c23d73f2f
Kukko Miikka <a1700386@myy.haaga-helia.fi>
2018-01-31 09:39:02 +0200

    w1t2 finished

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,10 +13,23 @@ public class HelloController {
 	public String index() {
 		return "This is the main page";
 	}
-	
+
 	@RequestMapping("/contact")
 	public String contact() {
 		return "This is the contact page";
 	}
+	
+	@RequestMapping("/hello")
+	public String home(@RequestParam(value = "name") String name, @RequestParam(value = "location") String location, Model model) {
+		model.addAttribute("name",name);
+		// do something
+		return "welcome to the "+location+" "+name;
+	}
+	
+	/*public String homea(@RequestParam(value = "location") String location, Model model) {
+		model.addAttribute("location",location);
+		// do something
+		return "hello "+location;
+	}*/
 
 }
