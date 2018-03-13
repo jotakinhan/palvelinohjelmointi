c75d246b0dc4b6c3a10c0e5ce2195a68f839e7b0
unknown <jotakinhan@gmail.com>
2018-03-13 23:31:12 +0200

    kaikki toimii

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.domain.Student;
 import com.example.demo.domain.Student2;
@@ -17,27 +18,31 @@ import com.example.demo.domain.StudentRepository;
 
 @Controller
 public class HelloController {
-	
+
 	@Autowired
 	private StudentRepository repository;
-	
+
 	@RequestMapping("/index")
+	@ResponseBody
 	public String index() {
 		return "This is the main page";
 	}
 
 	@RequestMapping("/contact")
+	@ResponseBody
 	public String contact() {
 		return "This is the contact page";
 	}
-	
+
 	@RequestMapping("/hello")
-	public String home(@RequestParam(value = "name") String name, @RequestParam(value = "location") String location, Model model) {
-		model.addAttribute("name",name);
+	@ResponseBody
+	public String home(@RequestParam(value = "name") String name, @RequestParam(value = "location") String location,
+			Model model) {
+		model.addAttribute("name", name);
 		// do something
-		return "welcome to the "+location+" "+name;
+		return "welcome to the " + location + " " + name;
 	}
-	
+
 	@RequestMapping("/agelimit")
 	public String hello(@RequestParam(value = "name") String name, @RequestParam(value = "age") String age,
 			Model model) {
@@ -68,19 +73,47 @@ public class HelloController {
 		model.addAttribute("students", oppilasLista);
 		return "list";
 	}
-	
-	@RequestMapping(value = "/list2")
-	public String studentlist2(Model model) {
-		List<Student2> students = repository.findAll();
-		model.addAttribute("students", students);
-		/*model.addAttribute("students", repository.findAll());*/
+
+	List<String> frList = new ArrayList<String>();
+
+	@RequestMapping("/list2")
+	public String index(@RequestParam(value = "friend", defaultValue = "") String name, Model model) {
+		if (name.length() > 0) {
+			frList.add(name);
+		}
+		model.addAttribute("friends", frList);
 		return "list2";
 	}
-	
-	@RequestMapping(value = "/save/{firstName}", method = RequestMethod.POST)
-    public String save(@PathVariable("firstName") String firstName, Student student){
-        //repository.save(student);
-        return "redirect:list2";
-    }
+
+	@RequestMapping(value = "/list3")
+	public String bookList(Model model) {
+		List<Student2> books = repository.findAll();
+		Student2 oppilas1 = new Student2();
+		oppilas1.setFirstName("Kate");
+		oppilas1.setLastName("Cole");
+		oppilas1.setEmail("kateemail");
+		books.add(oppilas1);
+		model.addAttribute("student2s", books);
+		model.addAttribute("student2s", new Student2());
+		return "list3";
+	}
+
+	@RequestMapping(value = "/add")
+	public String addStudent2(Model model) {
+		model.addAttribute("student", new Student2());
+		return "add";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Student2 student) {
+		repository.save(student);
+		return "redirect:list2";
+	}
+
+	/*
+	 * @RequestMapping(value = "/save/{firstName}", method = RequestMethod.POST)
+	 * public String save(@PathVariable("firstName") String firstName, Student
+	 * student){ //repository.save(student); return "redirect:list2"; }
+	 */
 
 }
